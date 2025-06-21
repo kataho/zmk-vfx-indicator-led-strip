6e839f9d595c1756b9c784574f3682bbb0582b70
kataho <kataho.mail@gmail.com>
2025-06-22 02:34:40 +0900

    make blink animation normal animation type configuration for animation mode on idle and active state in kconfig removed

--- src/indicator_led_strip.c
+++ src/indicator_led_strip.c
@@ -1,6 +1,5 @@
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
-#include <zephyr/drivers/led.h>
 #include <zephyr/init.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
@@ -18,10 +17,8 @@
 #include <zmk/events/ble_active_profile_changed.h>
 #include <zmk/events/layer_state_changed.h>
 #include <zmk/events/activity_state_changed.h>
-#include <zmk/events/split_peripheral_status_changed.h>
 #include <zmk/workqueue.h>
 #include <zmk/keymap.h>
-#include <zmk/split/bluetooth/peripheral.h>
 
 #include <zmk_indicator_led_strip/indicator_led_strip.h>
 
@@ -33,22 +30,39 @@
 #define BLUET_INDEX 1
 #define LAYER_INDEX 2
 
-#define POWER_COLOR_FULL   ((RGB){r: 0,   g: 1.0, b: 0.3})
-#define POWER_COLOR_HALF   ((RGB){r: 0,   g: 1.0, b: 0})
-#define POWER_COLOR_EMPTY  ((RGB){r: 1.0, g: 0,   b: 0})
+#define POWER_COLOR_100  ((RGB){r: 0,   g: 1.0, b: 0.3})
+#define POWER_COLOR_90   ((RGB){r: 0,   g: 1.0, b: 0.3})
+#define POWER_COLOR_80   ((RGB){r: 0,   g: 1.0, b: 0.3})
+#define POWER_COLOR_70   ((RGB){r: 0,   g: 1.0, b: 0.3})
+#define POWER_COLOR_60   ((RGB){r: 0,   g: 1.0, b: 0.3})
+#define POWER_COLOR_50   ((RGB){r: 0,   g: 1.0, b: 0.3})
+#define POWER_COLOR_40   ((RGB){r: 0,   g: 1.0, b: 0.3})
+#define POWER_COLOR_30   ((RGB){r: 0,   g: 1.0, b: 0})
+#define POWER_COLOR_20   ((RGB){r: 0,   g: 1.0, b: 0})
+#define POWER_COLOR_10   ((RGB){r: 1.0, g: 0,   b: 0})
+#define POWER_COLOR_0    ((RGB){r: 1.0, g: 0,   b: 0})
 
 #define BLUET_COLOR_ADVERT ((RGB){r: 0, g: 0, b: 1.0})
 #define BLUET_COLOR_DISCON ((RGB){r: 0, g: 0, b: 0})
-#define BLUET_COLOR_P0 ((RGB){r: 1.0, g: 0.3, b: 0})
-#define BLUET_COLOR_P1 ((RGB){r: 0,   g: 0.8, b: 1.0})
-#define BLUET_COLOR_P2 ((RGB){r: 0.8, g: 0.8, b: 0})
-#define BLUET_COLOR_DEFAULT ((RGB){r: 1.0, g: 1.0, b: 1.0})
+#define BLUET_COLOR_P0  ((RGB){r: 1.0, g: 0.3, b: 0})
+#define BLUET_COLOR_P1  ((RGB){r: 0,   g: 0.8, b: 1.0})
+#define BLUET_COLOR_P2  ((RGB){r: 0.8, g: 0.8, b: 0})
+#define BLUET_COLOR_PX  ((RGB){r: 1.0, g: 1.0, b: 1.0})
 
 #define LAYER_COLOR_L0 ((RGB){r: 1.0, g: 0,   b: 0})
 #define LAYER_COLOR_L1 ((RGB){r: 0.4, g: 0,   b: 1.0})
 #define LAYER_COLOR_L2 ((RGB){r: 0,   g: 1.0, b: 0})
 #define LAYER_COLOR_L3 ((RGB){r: 0,   g: 0.4, b: 1.0})
-#define LAYER_COLOR_NULL ((RGB){r: 0, g: 0, b: 0})
+#define LAYER_COLOR_LX ((RGB){r: 0,   g: 0,   b: 0})
+
+#define BREATHE_LENGTH 5000
+
+#define BLINK_BRIGHTNESS 0.5f
+#define BLINK_OFF_LENGTH 2000
+#define BLINK_ON_LENGTH 20
+
+#define ACTIVE_MODE BREATHE
+#define IDLE_MODE BLINK
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
@@ -80,11 +94,8 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 enum indicator_mode {
     OFF = 0,
     SOLID,
-    BREATHE
-};
-
-struct indicator_led_strip_state {
-    enum indicator_mode mode;
+    BREATHE,
+    BLINK
 };
 
 typedef struct {
@@ -96,11 +107,38 @@ typedef struct {
 static const struct device *led_strip;
 static RGB indicators[STRIP_NUM_PIXELS];
 static RGB fractions[STRIP_NUM_PIXELS];
-static struct indicator_led_strip_state state;
+static enum indicator_mode state_mode;
+static uint32_t animation_counter = 0;
 
 #if IS_ENABLED(CONFIG_INDICATOR_LED_STRIP_EXT_POWER)
 static const struct device *const ext_power = DEVICE_DT_GET(DT_INST(0, zmk_ext_power_generic));
 #endif
+
+//// Ext-Power Operation
+
+inline static void ext_power_on() {
+#if IS_ENABLED(CONFIG_INDICATOR_LED_STRIP_EXT_POWER)
+    if (ext_power != NULL && device_is_ready(ext_power)) {
+        int rc = ext_power_enable(ext_power);
+        if (rc != 0) {
+            LOG_ERR("Unable to enable EXT_POWER: %d", rc);
+        }
+    } else {
+        LOG_ERR("ext power device is not ready");
+    }
+#endif
+}
+
+inline static void ext_power_off() {
+#if IS_ENABLED(CONFIG_INDICATOR_LED_STRIP_EXT_POWER)
+    if (ext_power != NULL) {
+        int rc = ext_power_disable(ext_power);
+        if (rc != 0) {
+            LOG_ERR("Unable to disable EXT_POWER: %d", rc);
+        }
+    }
+#endif
+}
 
 //// Timer setup for tick
 
@@ -125,40 +163,58 @@ static struct led_rgb convert_to_led_rgb(const RGB *const rgb, RGB *frac) {
     };
 }
 
-static void apply_breathe_effect(const RGB *const rgb, RGB *out_rgb) {
-    static uint32_t counter = 0;
-    const uint32_t freq = 20000;
-    const float M_PI_2 = 6.2831853f;
-    float breathe_rate = (sinf(M_PI_2 * ((float)(counter % freq) / (float)freq)) + 1.1f) * 0.45f ;
-    out_rgb->r = rgb->r * breathe_rate;
-    out_rgb->g = rgb->g * breathe_rate;
-    out_rgb->b = rgb->b * breathe_rate;
-    counter++;
+inline static void scale_rgb(const RGB *const rgb, RGB *out_rgb, float rate) {
+    out_rgb->r = rgb->r * rate;
+    out_rgb->g = rgb->g * rate;
+    out_rgb->b = rgb->b * rate;
 }
 
 static void indicator_tick(struct k_work *work) {
     RGB *target = indicators;
     RGB out_rgb[STRIP_NUM_PIXELS];
-    if (state.mode == BREATHE) {
-        apply_breathe_effect(&indicators[POWER_INDEX], &out_rgb[POWER_INDEX]);
-        apply_breathe_effect(&indicators[BLUET_INDEX], &out_rgb[BLUET_INDEX]);
-        apply_breathe_effect(&indicators[LAYER_INDEX], &out_rgb[LAYER_INDEX]);
+
+    if (state_mode == BLINK) {
+        const uint32_t blink_time = animation_counter % (BLINK_OFF_LENGTH + BLINK_ON_LENGTH);
+        float brightness;
+        if (blink_time < BLINK_OFF_LENGTH) {
+            if (blink_time == 0) ext_power_off();
+            brightness = 0;
+        } else {
+            if (blink_time == BLINK_OFF_LENGTH) ext_power_on();
+            brightness = BLINK_BRIGHTNESS;
+        }
+        scale_rgb(&indicators[POWER_INDEX], &out_rgb[POWER_INDEX], brightness);
+        scale_rgb(&indicators[BLUET_INDEX], &out_rgb[BLUET_INDEX], brightness);
+        scale_rgb(&indicators[LAYER_INDEX], &out_rgb[LAYER_INDEX], brightness);
+        target = out_rgb;
+    } else if (state_mode == BREATHE) {
+        #define M_PI_2 6.2831853f
+        float breathe_rate = (cosf(
+            M_PI_2 * ((float)(animation_counter % BREATHE_LENGTH) / (float)BREATHE_LENGTH)
+        ) + 1.1f) * 0.45f;
+        scale_rgb(&indicators[POWER_INDEX], &out_rgb[POWER_INDEX], breathe_rate);
+        scale_rgb(&indicators[BLUET_INDEX], &out_rgb[BLUET_INDEX], breathe_rate);
+        scale_rgb(&indicators[LAYER_INDEX], &out_rgb[LAYER_INDEX], breathe_rate);
         target = out_rgb;
     }
+
     struct led_rgb pixels[STRIP_NUM_PIXELS];
     pixels[POWER_INDEX] = convert_to_led_rgb(&target[POWER_INDEX], &fractions[POWER_INDEX]);
     pixels[BLUET_INDEX] = convert_to_led_rgb(&target[BLUET_INDEX], &fractions[BLUET_INDEX]);
     pixels[LAYER_INDEX] = convert_to_led_rgb(&target[LAYER_INDEX], &fractions[LAYER_INDEX]);
+
     int err = led_strip_update_rgb(led_strip, pixels, STRIP_NUM_PIXELS);
     if (err < 0) {
         LOG_ERR("Failed to update the RGB strip (%d)", err);
     }
+
+    animation_counter++;
 }
 
 K_WORK_DEFINE(indicator_work, indicator_tick);
 
 static void indicator_timer_cb(struct k_timer *timer) {
-    if (state.mode == OFF) return;
+    if (state_mode == OFF) return;
     k_work_submit_to_queue(zmk_workqueue_lowprio_work_q(), &indicator_work);
 }
 
@@ -166,89 +222,72 @@ K_TIMER_DEFINE(indicator_timer, indicator_timer_cb, NULL);
 
 //// Toggle
 
-static int indicator_on(void) {
-    if (!led_strip) return -ENODEV;
-
-#if IS_ENABLED(CONFIG_INDICATOR_LED_STRIP_EXT_POWER)
-    if (ext_power != NULL && device_is_ready(ext_power)) {
-        int rc = ext_power_enable(ext_power);
-        if (rc != 0) {
-            LOG_ERR("Unable to enable EXT_POWER: %d", rc);
-        }
-    } else {
-        LOG_ERR("ext power device is not ready");
-    }
-#endif
-
-    state.mode = IS_ENABLED(CONFIG_INDICATOR_LED_STRIP_BREATHE) ? BREATHE : SOLID;
-    /* 4MHzで1bit ws2812の1bitはspiの8bitだから1usかかる計算
-    24*3+8(reset)で更新一回 80us なのでタイマー周期1msあたりから試してもいいかもね */
+static void animation_on(void) {
+    // 4MHzで1bit ws2812の1bitはspiの8bitだから1usかかる計算
+    // 24*3+8(reset)で更新一回 80us なのでタイマー周期1msあたりから試してもいいかも
     k_timer_start(&indicator_timer, K_NO_WAIT, K_MSEC(1));
+    animation_counter = 0;
 
-    return 0;
+    k_sleep(K_MSEC(3));
 }
 
-static void indicator_off_task(struct k_work *work) {
+static void animation_off_task(struct k_work *work) {
     struct led_rgb offpixels[STRIP_NUM_PIXELS];
     for (int i = 0; i < STRIP_NUM_PIXELS; i++) {
         offpixels[i] = (struct led_rgb){r : 0, g : 0, b : 0};
     }
     led_strip_update_rgb(led_strip, offpixels, STRIP_NUM_PIXELS);
 }
-K_WORK_DEFINE(indicator_off_work, indicator_off_task);
+K_WORK_DEFINE(animation_off_work, animation_off_task);
 
-static int indicator_off(void) {
-    if (!led_strip) return -ENODEV;
-
-#if IS_ENABLED(CONFIG_INDICATOR_LED_STRIP_EXT_POWER)
-    if (ext_power != NULL) {
-        int rc = ext_power_disable(ext_power);
-        if (rc != 0) {
-            LOG_ERR("Unable to disable EXT_POWER: %d", rc);
-        }
-    }
-#endif
-
-    k_work_submit_to_queue(zmk_workqueue_lowprio_work_q(), &indicator_off_work);
+static void animation_off(void) {
+    k_work_submit_to_queue(zmk_workqueue_lowprio_work_q(), &animation_off_work);
     k_timer_stop(&indicator_timer);
-    state.mode = OFF;
-
-    return 0;
 }
 
 int indicator_led_strip_toggle(void) {
-    switch (state.mode) {
+    if (!led_strip) return -ENODEV;
+
+    switch (state_mode) {
     case OFF:
-        state.mode = IS_ENABLED(CONFIG_INDICATOR_LED_STRIP_BREATHE) ? BREATHE : SOLID;
-        return indicator_on();
-    case SOLID:
-    case BREATHE:
-        return indicator_off();
+        state_mode = ACTIVE_MODE;
+        animation_on();
+        ext_power_on();
+    default:
+        state_mode = OFF;
+        ext_power_off();
+        animation_off();
     }
     return 0;
 }
 
-//// Listeners
+//// Battery Listener
+
+#define SET_INDICATOR_COLOR(CL, FL, VAL) \
+    if (CL > VAL && VAL >= FL) { \
+    const float rate_ = (VAL - FL) / 10.0f; \
+    indicators[POWER_INDEX].r = POWER_COLOR_##FL.r + (POWER_COLOR_##CL.r - POWER_COLOR_##FL.r) * rate_; \
+    indicators[POWER_INDEX].g = POWER_COLOR_##FL.g + (POWER_COLOR_##CL.g - POWER_COLOR_##FL.g) * rate_; \
+    indicators[POWER_INDEX].b = POWER_COLOR_##FL.b + (POWER_COLOR_##CL.b - POWER_COLOR_##FL.b) * rate_; }
 
 static int battery_listener_cb(const zmk_event_t *eh) {
-    if (state.mode == OFF) return 0;
+    if (state_mode == OFF) return 0;
 
-    float rate = ((eh != NULL)
+    int16_t rate = (eh != NULL)
         ? as_zmk_battery_state_changed(eh)->state_of_charge
-        : zmk_battery_state_of_charge()
-    ) / 100.0f;
+        : zmk_battery_state_of_charge();
+    // avoid value "100"
+    rate = rate >= 100 ? 99 : rate;
 
-    if (rate > 0.5f) {
-        const float upper_rate = (rate - 0.5f) / 0.5f;
-        indicators[POWER_INDEX].r = POWER_COLOR_HALF.r + (POWER_COLOR_FULL.r - POWER_COLOR_HALF.r) * upper_rate;
-        indicators[POWER_INDEX].g = POWER_COLOR_HALF.g + (POWER_COLOR_FULL.g - POWER_COLOR_HALF.g) * upper_rate;
-        indicators[POWER_INDEX].b = POWER_COLOR_HALF.b + (POWER_COLOR_FULL.b - POWER_COLOR_HALF.b) * upper_rate;
-    } else {
-        const float lower_rate = rate / 0.5f;
-        indicators[POWER_INDEX].r = POWER_COLOR_EMPTY.r + (POWER_COLOR_HALF.r - POWER_COLOR_EMPTY.r) * lower_rate;
-        indicators[POWER_INDEX].g = POWER_COLOR_EMPTY.g + (POWER_COLOR_HALF.g - POWER_COLOR_EMPTY.g) * lower_rate;
-        indicators[POWER_INDEX].b = POWER_COLOR_EMPTY.b + (POWER_COLOR_HALF.b - POWER_COLOR_EMPTY.b) * lower_rate;
-    }
+    SET_INDICATOR_COLOR(100, 90, rate) else
+    SET_INDICATOR_COLOR(90, 80, rate) else
+    SET_INDICATOR_COLOR(80, 70, rate) else
+    SET_INDICATOR_COLOR(60, 50, rate) else
+    SET_INDICATOR_COLOR(50, 40, rate) else
+    SET_INDICATOR_COLOR(40, 30, rate) else
+    SET_INDICATOR_COLOR(30, 20, rate) else
+    SET_INDICATOR_COLOR(20, 10, rate) else
+    SET_INDICATOR_COLOR(10, 0, rate)
 
     return 0;
 }
@@ -256,16 +295,17 @@ static int battery_listener_cb(const zmk_event_t *eh) {
 ZMK_LISTENER(indicator_battery_listener, battery_listener_cb);
 ZMK_SUBSCRIPTION(indicator_battery_listener, zmk_battery_state_changed);
 
-static int bluetooth_listener_cb(const zmk_event_t *eh) {
-    if (state.mode == OFF) return 0;
+//// Bluetooth Listener
 
-    uint8_t profile_index = zmk_ble_active_profile_index();
+static int bluetooth_listener_cb(const zmk_event_t *eh) {
+    if (state_mode == OFF) return 0;
+
     if (zmk_ble_active_profile_is_connected()) {
-        switch (profile_index) {
-        case 0: indicators[BLUET_INDEX] = BLUET_COLOR_P0; break;
-        case 1: indicators[BLUET_INDEX] = BLUET_COLOR_P1; break;
-        case 2: indicators[BLUET_INDEX] = BLUET_COLOR_P2; break;
-        default: indicators[BLUET_INDEX] = BLUET_COLOR_DEFAULT; break;
+        switch (zmk_ble_active_profile_index()) {
+            case 0: indicators[BLUET_INDEX] = BLUET_COLOR_P0; break;
+            case 1: indicators[BLUET_INDEX] = BLUET_COLOR_P1; break;
+            case 2: indicators[BLUET_INDEX] = BLUET_COLOR_P2; break;
+            default: indicators[BLUET_INDEX] = BLUET_COLOR_PX; break;
         }
     } else if (zmk_ble_active_profile_is_open()) {
         indicators[BLUET_INDEX] = BLUET_COLOR_ADVERT;
@@ -278,15 +318,17 @@ static int bluetooth_listener_cb(const zmk_event_t *eh) {
 ZMK_LISTENER(indicator_bluetooth_listener, bluetooth_listener_cb);
 ZMK_SUBSCRIPTION(indicator_bluetooth_listener, zmk_ble_active_profile_changed);
 
+//// Layer Listener
+
 static int layer_listener_cb(const zmk_event_t *eh) {
-    if (state.mode == OFF) return 0;
+    if (state_mode == OFF) return 0;
 
     switch (zmk_keymap_highest_layer_active()) {
     case 0: indicators[LAYER_INDEX] = LAYER_COLOR_L0; break;
     case 1: indicators[LAYER_INDEX] = LAYER_COLOR_L1; break;
     case 2: indicators[LAYER_INDEX] = LAYER_COLOR_L2; break;
     case 3: indicators[LAYER_INDEX] = LAYER_COLOR_L3; break;
-    default: indicators[LAYER_INDEX] = LAYER_COLOR_NULL; break;
+    default: indicators[LAYER_INDEX] = LAYER_COLOR_LX; break;
     }
     return 0;
 }
@@ -294,15 +336,20 @@ static int layer_listener_cb(const zmk_event_t *eh) {
 ZMK_LISTENER(indicator_layer_listener, layer_listener_cb);
 ZMK_SUBSCRIPTION(indicator_layer_listener, zmk_layer_state_changed);
 
-#if IS_ENABLED(CONFIG_INDICATOR_LED_STRIP_OFF_IDLE)
+//// Activity State Listener
 
 static int activity_state_listener_cb(const zmk_event_t *eh) {
     switch (zmk_activity_get_state()) {
     case ZMK_ACTIVITY_ACTIVE:
-        indicator_on();
+        if (state_mode != ACTIVE_MODE) animation_counter = 0;
+        state_mode = ACTIVE_MODE;
+        if (IDLE_MODE == OFF) animation_on();
+        ext_power_on();
         break;
     case ZMK_ACTIVITY_IDLE:
-        indicator_off();
+        if (IDLE_MODE == OFF) animation_off();
+        if (state_mode != IDLE_MODE) animation_counter = 0;
+        state_mode = IDLE_MODE;
         break;
     default:
         break;
@@ -313,30 +360,35 @@ static int activity_state_listener_cb(const zmk_event_t *eh) {
 ZMK_LISTENER(indicator_activity_state_listener, activity_state_listener_cb);
 ZMK_SUBSCRIPTION(indicator_activity_state_listener, zmk_activity_state_changed);
 
-#endif
-
 //// Initialization
 
-extern void indicator_led_strip_init(void) {
+static int indicator_led_strip_init(void) {
     led_strip = DEVICE_DT_GET(STRIP_CHOSEN);
 
-    state = (struct indicator_led_strip_state){
-        mode : OFF
-    };
+    state_mode = OFF;
 
-    indicators[POWER_INDEX] = POWER_COLOR_EMPTY;
+    indicators[POWER_INDEX] = POWER_COLOR_0;
     indicators[BLUET_INDEX] = BLUET_COLOR_DISCON;
-    indicators[LAYER_INDEX] = LAYER_COLOR_NULL;
+    indicators[LAYER_INDEX] = LAYER_COLOR_LX;
 
+    return 0;
+}
+
+SYS_INIT(indicator_led_strip_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
+
+extern void indicator_led_strip_init_lazy(void) {
     if (IS_ENABLED(CONFIG_INDICATOR_LED_STRIP_ON_START)) {
-        indicator_on();
+        state_mode = ACTIVE_MODE;
+        animation_on();
+        ext_power_on();
     }
 
     k_sleep(K_MSEC(200));
+
     battery_listener_cb(NULL);
     layer_listener_cb(NULL);
     bluetooth_listener_cb(NULL);
 }
 
-K_THREAD_DEFINE(indicator_led_strip_init_tid, 1024, indicator_led_strip_init, NULL, NULL, NULL,
+K_THREAD_DEFINE(init_thread, 1024, indicator_led_strip_init_lazy, NULL, NULL, NULL,
                 K_LOWEST_APPLICATION_THREAD_PRIO, 0, 200);
